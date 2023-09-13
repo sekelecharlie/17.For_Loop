c3d469cdf75f46413cfb0d019ab171a119bf6712
Dell-User <Dell-User@DESKTOP-HCCU5E8>
2023-09-13 11:16:58 +0200

    Add project files.

---
+++ 17.For_Loop/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17.For_Loop
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("This is the For loop ");
+            for (int i = 0; i <= 10; i++)
+            {
+                Console.WriteLine(i);
+                Console.WriteLine("Hi, we are looping");
+                Console.WriteLine("This is i+2 = " +Convert.ToInt32(i +2));
+            }
+            Console.WriteLine();
+            Console.WriteLine("**********");
+            Console.WriteLine("End OF FOR LOOP");
+            Console.WriteLine("**************");
+            Console.WriteLine();
+            Console.WriteLine("This is the While loop ");
+            int j = 0;
+            while (j <= 5)
+            {
+                Console.WriteLine(j);
+                j++;
+            }
+            Console.ReadLine();
+        }
+    }
+}
